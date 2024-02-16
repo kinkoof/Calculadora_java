81a34dd0e61842fbdcaf553752d3f4da6434b5c9
lucas_ferrari <lucasdesouzaferrari@gmail.com>
2024-02-16 20:30:04 -0300

    modified:   Calculadora/bin/App.class 	modified:   Calculadora/bin/MainPanel.class 	modified:   Calculadora/bin/Operacoes.class 	modified:   Calculadora/src/MainPanel.java 	modified:   Calculadora/src/Operacoes.java

--- Calculadora/src/MainPanel.java
+++ Calculadora/src/MainPanel.java
@@ -1,152 +1,192 @@
-import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JTextPane;
+import javax.swing.JTextField;
 
-public class MainPanel extends JPanel implements ActionListener {
+public class MainPanel implements ActionListener {
 
-    JTextPane text;
-    JButton botao1;
-    JButton botao2;
-    JButton botao3;
-    JButton botao4;
-    JButton botao5;
-    JButton botao6;
-    JButton botao7;
-    JButton botao8;
-    JButton botao9;
-    JButton botao0;
-    JButton botao;
-    JButton botaoMais;
-    JButton botaoMenos;
-    JButton botaoDivisao;
-    JButton botaoMult;
-    JButton botaoIgual;
-    JButton botaoClean;
+    JFrame frame;
+    JTextField text;
+    JButton[] numButtons = new JButton[10];
+    JButton[] funcButtons = new JButton[9];
+    JButton add, sub, mul, div, dec, equal, del, clear, neg;
+    JPanel panel;
 
-    int num;
-    String currentText;
+    double num1 = 0, num2 = 0, result = 0;
+    char operador;
+
+    Font font = new Font("Ink Free", Font.BOLD, 30);
 
     MainPanel() {
+        frame = new JFrame("Calculadora em Java");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 550);
+        frame.setLayout(null);
 
-        botao1 = new JButton("1");
-        botao1.addActionListener(this);
-        botao2 = new JButton("2");
-        botao2.addActionListener(this);
-        botao3 = new JButton("3");
-        botao3.addActionListener(this);
-        botao4 = new JButton("4");
-        botao4.addActionListener(this);
-        botao5 = new JButton("5");
-        botao5.addActionListener(this);
-        botao6 = new JButton("6");
-        botao6.addActionListener(this);
-        botao7 = new JButton("7");
-        botao7.addActionListener(this);
-        botao8 = new JButton("8");
-        botao8.addActionListener(this);
-        botao9 = new JButton("9");
-        botao9.addActionListener(this);
-        botao0 = new JButton("0");
-        botao0.addActionListener(this);
-        botaoMais = new JButton("+");
-        botaoMais.addActionListener(this);
-        botaoMenos = new JButton("-");
-        botaoMenos.addActionListener(this);
-        botaoDivisao = new JButton("÷");
-        botaoDivisao.addActionListener(this);
-        botaoMult = new JButton("x");
-        botaoMult.addActionListener(this);
-        botaoIgual = new JButton("=");
-        botaoIgual.addActionListener(this);
-        botaoClean = new JButton("C");
-        botaoClean.addActionListener(this);
+        text = new JTextField();
+        text.setBounds(50, 25, 300, 50);
+        text.setFont(font);
+        text.setEditable(false);
 
-        JButton[] botoes = { botao7, botao8, botao9, botaoMult, botao4, botao5, botao6, botaoMenos, botao1, botao2,
-                botao3, botaoMais, botaoClean, botao0, botaoIgual, botaoDivisao };
+        add = new JButton("+");
+        sub = new JButton("-");
+        mul = new JButton("X");
+        div = new JButton("÷");
+        dec = new JButton(".");
+        equal = new JButton("=");
+        del = new JButton("Delete");
+        clear = new JButton("Clear");
+        neg = new JButton("(-)");
 
-        JPanel panelTop = new JPanel();
-        JPanel panelBot = new JPanel();
+        funcButtons[0] = add;
+        funcButtons[1] = sub;
+        funcButtons[2] = mul;
+        funcButtons[3] = div;
+        funcButtons[4] = dec;
+        funcButtons[5] = equal;
+        funcButtons[6] = del;
+        funcButtons[7] = clear;
+        funcButtons[8] = neg;
 
-        panelTop.setBackground(Color.GREEN);
-        panelTop.setLayout(new GridLayout());
-        panelTop.setPreferredSize(new Dimension(200, 100));
-        text = new JTextPane();
-        text.setFont(new Font("Consolas", Font.PLAIN, 100));
-        text.setForeground(Color.green);
-        text.setBackground(Color.BLACK);
-        text.setCaretColor(Color.green);
-
-        panelTop.add(text);
-
-        panelBot.setBackground(Color.blue);
-        panelBot.setPreferredSize(new Dimension(200, 350));
-        panelBot.setLayout(new GridLayout(4, 4, 2, 2));
-
-        for (JButton buttons : botoes) {
-            panelBot.add(buttons);
+        for (JButton botao : funcButtons) {
+            botao.addActionListener(this);
+            botao.setFont(font);
+            botao.setFocusable(false);
 
         }
 
-        this.setOpaque(true);
-        this.setBackground(Color.black);
-        this.setLayout(new BorderLayout(10, 10));
-        this.add(panelTop, BorderLayout.NORTH);
-        this.add(panelBot, BorderLayout.SOUTH);
+        for (int i = 0; i < numButtons.length; i++) {
+            numButtons[i] = new JButton(String.valueOf(i));
+            numButtons[i].addActionListener(this);
+            numButtons[i].setFont(font);
+            numButtons[i].setFocusable(false);
+        }
+
+        del.setBounds(50, 430, 100, 50);
+        clear.setBounds(150, 430, 100, 50);
+        neg.setBounds(250,430,100,50);
+
+        panel = new JPanel();
+        panel.setBounds(50, 100, 300, 300);
+        panel.setLayout(new GridLayout(4, 4, 10, 10));
+
+        panel.add(numButtons[1]);
+        panel.add(numButtons[2]);
+        panel.add(numButtons[3]);
+        panel.add(add);
+        panel.add(numButtons[4]);
+        panel.add(numButtons[5]);
+        panel.add(numButtons[6]);
+        panel.add(sub);
+        panel.add(numButtons[7]);
+        panel.add(numButtons[8]);
+        panel.add(numButtons[9]);
+        panel.add(mul);
+        panel.add(dec);
+        panel.add(numButtons[0]);
+        panel.add(equal);
+        panel.add(div);
+
+        frame.add(panel);
+        frame.add(neg);
+        frame.add(clear);
+        frame.add(del);
+        frame.add(text);
+        frame.setVisible(true);
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == botao1) {
-            currentText = text.getText();
-            text.setText(currentText + "1");
-        } else if (e.getSource() == botao2) {
-            currentText = text.getText();
-            text.setText(currentText + "2");
-        } else if (e.getSource() == botao3) {
-            currentText = text.getText();
-            text.setText(currentText + "3");
-        } else if (e.getSource() == botao4) {
-            currentText = text.getText();
-            text.setText(currentText + "4");
-        } else if (e.getSource() == botao5) {
-            currentText = text.getText();
-            text.setText(currentText + "5");
-        } else if (e.getSource() == botao6) {
-            currentText = text.getText();
-            text.setText(currentText + "6");
-        } else if (e.getSource() == botao7) {
-            currentText = text.getText();
-            text.setText(currentText + "7");
-        } else if (e.getSource() == botao8) {
-            currentText = text.getText();
-            text.setText(currentText + "8");
-        } else if (e.getSource() == botao9) {
-            currentText = text.getText();
-            text.setText(currentText + "9");
-        } else if (e.getSource() == botao0) {
-            currentText = text.getText();
-            text.setText(currentText + "0");
-        } else if (e.getSource() == botaoMais) {
-            currentText = text.getText();
-            text.setText("+");
-        } else if (e.getSource() == botaoMenos) {
-            currentText = text.getText();
-            text.setText("-");
-        } else if (e.getSource() == botaoMult) {
-            currentText = text.getText();
-            text.setText("x");
-        } else if (e.getSource() == botaoDivisao) {
-            currentText = text.getText();
-            text.setText("/");
+        for (int i = 0; i < numButtons.length; i++) {
+            if (e.getSource() == numButtons[i]) {
+                text.setText(text.getText().concat(String.valueOf(i)));
+            }
+        }
+        if (e.getSource() == dec) {
+            text.setText(text.getText().concat("."));
+        }
+        if (e.getSource() == add) {
+            text.setText(text.getText());
+            num1 = Double.parseDouble(text.getText());
+            operador = '+';
+            text.setText("");
+        }
+        if (e.getSource() == sub) {
+            text.setText(text.getText());
+            num1 = Double.parseDouble(text.getText());
+            operador = '-';
+            text.setText("");
+        }
+        if (e.getSource() == mul) {
+            text.setText(text.getText());
+            num1 = Double.parseDouble(text.getText());
+            operador = '*';
+            text.setText("");
+        }
+        if (e.getSource() == div) {
+            text.setText(text.getText());
+            num1 = Double.parseDouble(text.getText());
+            operador = '/';
+            text.setText("");
+        }
+        if (e.getSource() == equal) {
+            num2 = Double.parseDouble(text.getText());
+
+            switch (operador) {
+
+                case '+':
+                    result = num1 + num2;
+                    break;
+
+                case '-':
+                    result = num1 - num2;
+                    break;
+
+                case '/':
+                    result = num1 / num2;
+                    break;
+
+                case '*':
+                    result = num1 * num2;
+                    break;
+
+                default:
+                    break;
+            }
+
+            text.setText(String.valueOf(result));
+            num1 = result;
+
         }
 
+        if (e.getSource() == clear) {
+            text.setText("");
+        }
+
+        if (e.getSource() == del) {
+            String str = text.getText();
+            text.setText("");
+            for (int i = 0; i < str.length() - 1; i++) {
+                text.setText(text.getText() + str.charAt(i));
+            }
+        }
+
+        if (e.getSource() == neg) {
+            double temp = Double.parseDouble(text.getText());
+            temp *= -1;
+            text.setText(String.valueOf(temp));
+        }
     }
+
+    public static void main(String[] args) {
+        new MainPanel();
+    }
+
 }
--- Calculadora/src/Operacoes.java
+++ Calculadora/src/Operacoes.java
@@ -1,7 +1,12 @@
-public class Operacoes {
+public class Operacoes extends MainPanel{
 
-    void soma(int x, int y){
-
+    int x;
+    int y;
+    void Soma(int x , int y){
+        int z = x + y;
+        String resposta = String.valueOf(z);
+        text.setText(resposta);
+        System.out.println(z);
     }
 }
 
